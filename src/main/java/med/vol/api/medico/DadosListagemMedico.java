a9be70066e98b121defd1d27689afbdc9245b341
Roberto Carnaúba Dias <betocarnauba@hotmail.com>
2023-01-08 08:34:03 -0300

    Ajuste na dto de listagem

--- src/main/java/med/vol/api/medico/DadosListagemMedico.java
+++ src/main/java/med/vol/api/medico/DadosListagemMedico.java
@@ -2,10 +2,10 @@ package med.vol.api.medico;
 
 import med.vol.api.endereco.Endereco;
 
-public record DadosListagemMedico(Long id,String nome, String email, String telefone, String crm, Especialidade especialidade, Endereco endereco) {
+public record DadosListagemMedico(Long id,String nome, String email, String telefone, String crm, Especialidade especialidade) {
 
 	public DadosListagemMedico(Medico medico) {
-		this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getTelefone(), medico.getCrm(), medico.getEspecialidade(), medico.getEndereco());
+		this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getTelefone(), medico.getCrm(), medico.getEspecialidade());
 	}
 
 }
